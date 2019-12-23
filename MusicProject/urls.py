4ef7f1a07ebc05b076b79cb78e1700e3a8cffe2b
weiting9199 <45114753+weiting9199@users.noreply.github.com>
2019-12-23 21:19:05 +0800

    move artist_image to static\images

--- MusicProject/urls.py
+++ MusicProject/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from routes.views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
     path('songlist/', songlist),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
