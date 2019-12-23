e44accf72b55b830148cf1f55686c5c847031131
weiting9199 <45114753+weiting9199@users.noreply.github.com>
2019-12-23 20:19:46 +0800

    fix the problem of runserver, but I don't know why

--- MusicProject/settings.py
+++ MusicProject/settings.py
@@ -126,3 +126,6 @@ STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder"
 )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
