c05b33538cd3bad263f8b1aa8529777d6082caf1
gnotnuyil <s2568943@gmail.com>
2019-12-29 14:20:50 +0800

    views.py

--- MusicProject/urls.py
+++ MusicProject/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
     path('songlist/', songlist),
-    path('artiistpage/', artistpage),
+    path('artistpage/', artistpage),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
